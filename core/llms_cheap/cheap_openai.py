e0da782e00a540d4c8a501ae97c28ae9c5b3d83a
wxy2ab <wxy2ab@sina.com>
2024-07-20 13:17:33 +0800

    cheap_openai

--- core/llms_cheap/cheap_openai.py
+++ core/llms_cheap/cheap_openai.py
@@ -3,5 +3,5 @@ from core.llms.openai_client import OpenAIClient
 
 class CheapOpenai(OpenAIClient):
 
-    def __init__(self,api_kek:str=""):
-        super().__init__(self,api_key=api_kek model="gpt-4o-mini")
+    def __init__(self,api_key:str=""):
+        super().__init__(self,api_key=api_key, model="gpt-4o-mini")
