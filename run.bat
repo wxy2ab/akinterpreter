13fe4823e7ff72adb90b5f08a5c097740f4d0c4d
wxy2ab <wxy2ab@sina.com>
2024-07-23 19:06:48 +0800

    fix run.bat bug

--- run.bat
+++ run.bat
@@ -2,9 +2,7 @@
 
 if not exist ".\env" (
     echo Environment not found. Installing Miniconda...
-    pushd .\install
-    call install.bat
-    popd
+    call .\install\install.bat
     if errorlevel 1 (
         echo Installation failed. Please check the error messages above.
         exit /b 1
