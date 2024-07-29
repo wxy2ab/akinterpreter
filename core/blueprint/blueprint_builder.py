283f264eeac373a9ee994496903c0d2ba618e4a0
wxy2ab <wxy2ab@sina.com>
2024-07-29 17:51:42 +0800

    llm provider

--- core/blueprint/blueprint_builder.py
+++ core/blueprint/blueprint_builder.py
@@ -2,15 +2,18 @@
 
 from typing import Any, Dict, Generator
 
+from .llm_provider import LLMProvider
+
 from .step_model_collection import StepModelCollection
 from .step_info_provider import StepInfoProvider
 
 
 class BluePrintBuilder:
     def __init__(self):
+        self.llm_provider = LLMProvider()
         self.provider = StepInfoProvider() 
         self.blueprint=StepModelCollection()
-    
+
     def build_blueprint(self, query: str) -> Generator[Dict[str, Any], None, None]:
         pass
     def modify_blueprint(self, query: str) -> Generator[Dict[str, Any], None, None]:
