e377e91ea9ff68aa0f1a26e60281c6cfb6c35b88
wxy2ab <wxy2ab@sina.com>
2024-07-30 14:15:50 +0000

    add new step files

---
+++ core/blueprint/data_analysis_step_executor.py
@@ -0,0 +1,4 @@
+from ._step_abstract import StepExecutor
+
+class DataAnalysisStepExecutor(StepExecutor):
+    pass
---
+++ core/blueprint/data_retrieval_step_executor.py
@@ -0,0 +1,5 @@
+from ._step_abstract import StepExecutor
+
+
+class DataRetrievalStepExecutor(StepExecutor):
+    pass
