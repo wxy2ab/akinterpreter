b59ce797a3d5f8c9e8464ef3e1ce6b543597fc27
wxy2ab <wxy2ab@sina.com>
2024-07-31 09:26:12 +0000

    update local embedding and reranker model

--- core/embeddings/bge_m3_embedding.py
+++ core/embeddings/bge_m3_embedding.py
@@ -6,7 +6,16 @@ class BGEM3Embedding(Embedding):
     def __init__(self):
         from ..utils.get_sentence_device import get_sentence_transformer_device
         device = get_sentence_transformer_device()
-        self.model = SentenceTransformer('BAAI/bge-m3',device=device)
+        from ..utils.config_setting import Config
+        api_key = ""
+        config = Config()
+        if config.has_key("hugging_face_api_key"):
+            api_key = config.get("hugging_face_api_key")
+        # 设置 API Key
+        if api_key:
+            import os
+            os.environ['HUGGING_FACE_HUB_TOKEN'] = api_key
+        self.model = SentenceTransformer('BAAI/bge-m3',device=device,token=api_key)
 
     def convert_to_embedding(self, input_strings: List[str]) -> List[List[float]]:
         return self.model.encode(input_strings).tolist()
--- core/embeddings/bge_reranker.py
+++ core/embeddings/bge_reranker.py
@@ -6,6 +6,15 @@ class BGEReranker(Ranker):
     def __init__(self,max_length:int=1024):
         from ..utils.get_sentence_device import get_sentence_transformer_device
         device = get_sentence_transformer_device()
+        from ..utils.config_setting import Config
+        api_key = ""
+        config = Config()
+        if config.has_key("hugging_face_api_key"):
+            api_key = config.get("hugging_face_api_key")
+        # 设置 API Key
+        if api_key:
+            import os
+            os.environ['HUGGING_FACE_HUB_TOKEN'] = api_key
         self.model = CrossEncoder('BAAI/bge-reranker-v2-m3',device=device,max_length=max_length)
 
     def get_scores(self, pairs:List[List[str]]) -> List[List[float]]:
