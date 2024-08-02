20b15682069ff772d60e5973f323e5569c898b7c
wxy2ab <wxy2ab@sina.com>
2024-08-02 21:56:13 +0800

    new structure basic code

--- core/blueprint/akshare_data_retrieval_step_model.py
+++ core/blueprint/akshare_data_retrieval_step_model.py
@@ -1,14 +1,11 @@
-
-
-from typing import Any, Dict, Literal
-
+from typing import Any, Dict, Literal, ClassVar
 from pydantic import Field
 from ._base_step_model import BaseStepModel
 from .data_retrieval_step_model import DataRetrievalStepModel
 
 class AkShareDataRetrievalStepModel(DataRetrievalStepModel):
     library: Literal['akshare'] = 'akshare'
-    step_type: Literal['data_retrieval'] = 'akshare_data_retrieval'
+    step_type: ClassVar[Literal['akshare_data_retrieval']] = Field('akshare_data_retrieval', allow_mutation=False)
     
     def to_dict(self) -> Dict[str, Any]:
         return {
