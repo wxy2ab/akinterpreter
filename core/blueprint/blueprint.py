2c3ef18e71a45a6a9433152c3e2a4fd7fc4affd6
wxy2ab <wxy2ab@sina.com>
2024-08-02 08:46:48 +0800

    new structure basic code

--- core/blueprint/blueprint.py
+++ core/blueprint/blueprint.py
@@ -5,14 +5,15 @@ from .blueprint_coder import BluePrintCoder
 from .blueprint_executor import BluePrintExecutor
 from .blueprint_reporter import BluePrintReporter
 from .step_model_collection import StepModelCollection
+from tenacity import retry, stop_after_attempt
 
 class BluePrint:
     def __init__(self):
         self.blueprint_builder = BluePrintBuilder()
-        self.blueprint_coder = None
-        self.blueprint_executor =None
-        self.blueprint_reporter = None
-        self._blueprint =None
+        self.blueprint_coder:BluePrintCoder = None
+        self.blueprint_executor:BluePrintExecutor =None
+        self.blueprint_reporter:BluePrintReporter = None
+        self._blueprint:StepModelCollection =None
         self.max_retry = 3
         self.step_data = StepData()
     
@@ -38,6 +39,8 @@ class BluePrint:
         for step in self._blueprint:
             yield from self.blueprint_coder.generate_step(step)
             yield from self.blueprint_executor.excute_step(step)
+    
+    def final_report(self)->Generator[dict[str,any],None,None]:
         if self.blueprint_reporter is None:
             self.blueprint_reporter = BluePrintReporter(self._blueprint,self.step_data)
         yield from self.blueprint_reporter.report()
