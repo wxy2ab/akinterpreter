08df2cabea76b3886a89a9d7a932e2b7ebdfb70a
wxy2ab <wxy2ab@sina.com>
2024-08-26 23:37:21 +0800

    modify config_setting

--- core/utils/config_setting.py
+++ core/utils/config_setting.py
@@ -1,33 +1,47 @@
-import configparser
-try:
-    from  .single_ton  import  Singleton
-except:
-    from single_ton import Singleton
 import os
+import configparser
 
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
 
-class Config(metaclass = Singleton):
+class Config(metaclass=Singleton):
     def __init__(self, path="setting.ini"):
         self.file_name = path
-        if not os.path.exists(path):
-            with open(path, "w") as file:
-                pass
         self.config = configparser.ConfigParser()
-        self.config.read(path,encoding='utf8')
         
-    def get(self,  key:str="token" ,/, section:str="Default"):
-        if not self.config.has_section(section):
-            self.config.add_section(section)
-        return self.config[section][key]
+        if os.path.exists(path):
+            self.config.read(path, encoding='utf8')
+            self.use_file = True
+        else:
+            self.use_file = False
 
-    def has_key(self, key:str="token" ,/, section:str="Default"):
-        if not self.config.has_section(section):
-            self.config.add_section(section)
-        return key in self.config[section]
+    def get(self, key: str = "token", /, section: str = "Default"):
+        if self.use_file:
+            if not self.config.has_section(section):
+                self.config.add_section(section)
+            return self.config[section].get(key)
+        else:
+            return os.environ.get(key.upper())
 
-    def set(self, key:str="token", value="" ,/, section:str="Default"):
-        if not self.config.has_section(section):
-            self.config.add_section(section)
-        self.config[section][key] = value
-        with open(self.file_name, "w") as configfile:
-            self.config.write(configfile)
+    def has_key(self, key: str = "token", /, section: str = "Default"):
+        if self.use_file:
+            if not self.config.has_section(section):
+                self.config.add_section(section)
+            return key in self.config[section]
+        else:
+            return key.upper() in os.environ
+
+    def set(self, key: str = "token", value: str = "", /, section: str = "Default"):
+        if self.use_file:
+            if not self.config.has_section(section):
+                self.config.add_section(section)
+            self.config[section][key] = value
+            with open(self.file_name, "w") as configfile:
+                self.config.write(configfile)
+        else:
+            # 注意：在非文件模式下，set 方法不会实际修改环境变量
+            print(f"Warning: Cannot set {key} = {value} in environment variable mode")
