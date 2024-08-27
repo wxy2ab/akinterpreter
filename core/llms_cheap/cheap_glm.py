850b5257e610c24ab23d3c30ea8992f2acf0a114
wxy2ab <wxy2ab@sina.com>
2024-08-27 17:50:42 +0800

    modify config_setting

--- core/llms_cheap/cheap_glm.py
+++ core/llms_cheap/cheap_glm.py
@@ -3,4 +3,4 @@ from ..llms.glm_client import GLMClient
 class CheapGLM(GLMClient):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.model = "GLM-4-Air" #GLM-4-Flash
+        self.model = "GLM-4-Flash" #GLM-4-Flash
